dfcbfd879bb0bf64734b867644c48ed7a66bf0f5
ahmedmatem <ahmedmatem@gmail.com>
2014-05-24 22:48:07 +0300

    enable registration

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -60,11 +60,11 @@ namespace TMC.Web.Controllers
         //
         // GET: /Account/Register
 
-        //[AllowAnonymous]
-        //public ActionResult Register()
-        //{
-        //    return View();
-        //}
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
 
         //
         // POST: /Account/Register
