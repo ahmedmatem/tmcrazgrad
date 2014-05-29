39d665bfc1b5ef178731fba8f0ee67c28c6332c4
ahmedmatem <ahmedmatem@gmail.com>
2014-05-29 11:32:26 +0300

    update client side thetre history

---
+++ Controllers/DT/TheatreController.cs
@@ -0,0 +1,39 @@
+﻿namespace TMC.Web.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using TMC.Models;
+    using TMC.Web.Models.DT.Theatre;
+    using TMC.Data;
+
+    public class TheatreController : BaseController
+    {
+        //
+        // GET: /Theatre/DTHistory
+
+        public ActionResult DTHistory()
+        {
+            HistoryViewModel model = new HistoryViewModel();
+            History history = new TmcDAL(this.tmcContext).GetHistoryByBranch(Branch.DT);
+
+            if (history == null)
+            {
+                history = new History();
+            }
+
+            if (history.HasImage)
+            {
+                model.Images = history.Images.ToList();
+            }
+
+            model.Content = history.Content;
+            model.Cloudinary = this.cloudinary;
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Models/DT/Theatre/HistoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace TMC.Web.Models.DT.Theatre
+{
+    using System.Web;
+    using System.Collections.Generic;
+
+    using CloudinaryDotNet;
+    using TMC.Models;
+
+    public class HistoryViewModel
+    {
+        public int HistoryId { get; set; }
+
+        public int Title { get; set; }
+
+        public string Content { get; set; }
+
+        public List<Image> Images { get; set; }
+
+        public Cloudinary Cloudinary { get; set; }
+    }
+}
